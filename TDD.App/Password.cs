435bea76d42be7bfb622d3600d9a6ba002b3f34f
Crisitian Llanos <llanosomar123@hotmail.com>
2025-04-29 08:49:24 -0500

    Cristian Llanos

---
+++ TDD.App/Password.cs
@@ -0,0 +1,55 @@
+﻿namespace TDD.App
+{
+    public class Password
+    {
+        public bool VerificarNumero (string password)
+        {
+            bool numero = false;
+            for (int i = 0; i < password.Length; i++)
+            {
+                if(char.IsDigit(password[i])) 
+                    numero = true;
+            }
+
+            return numero;
+        }
+
+        public bool VerificarLongitud(string password)
+        {
+            return password.Length >= 8;
+        }
+
+        public bool VerificarMayuscula(string password)
+        {
+            bool mayuscula = false;
+
+            for(int i = 0;i < password.Length; i++)
+            {
+                if(char.IsAsciiLetterUpper(password[i]))
+                    mayuscula = true;
+            }
+
+            return mayuscula;
+        }
+
+        public bool VerificarSimbolo(string password)
+        {
+            bool simbolo = false;
+            for (int i = 0; i < password.Length; i++)
+            {
+                if(char.IsPunctuation(password[i]))
+                    simbolo = true;
+            }
+
+            return simbolo;
+        }
+
+        public bool EsSegura(string password)
+        {
+            return VerificarNumero(password) &&
+                   VerificarLongitud(password) &&
+                   VerificarMayuscula(password) &&
+                   VerificarSimbolo(password);
+        }
+    }
+}
---
+++ TDD.Tests/PasswordTest.cs
@@ -0,0 +1,67 @@
+﻿using TDD.App;
+
+namespace TDD.Tests
+{
+    internal class PasswordTest
+    {
+        [Test]
+        public void EsSeguroCaso01()
+        {
+            var password = new Password();
+
+            var result = password.EsSegura("hola123");
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void EsSeguroCaso02()
+        {
+            var password = new Password();
+
+            var result = password.EsSegura("hola12");
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void EsSeguroCaso03()
+        {
+            var password = new Password();
+
+            var result = password.EsSegura("hola123");
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void EsSeguroCaso04()
+        {
+            var password = new Password();
+
+            var result = password.EsSegura("holaaaa");
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void EsSeguroCaso05()
+        {
+            var password = new Password();
+
+            var result = password.EsSegura("Hola123");
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void EsSeguroCaso06()
+        {
+            var password = new Password();
+
+            var result = password.EsSegura("C&ps2025");
+
+            Assert.IsTrue(result);
+        }
+    }
+}
